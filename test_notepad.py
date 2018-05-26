997be0b636f6856798a9a6eca5ef27da37279f96
gramb <Lukinvv@inbox.ru>
2018-05-26 21:40:31 +0300

    запуск notepad

--- test_notepad.py
+++ test_notepad.py
@@ -2,8 +2,10 @@ import clr
 import os.path
 import sys
 project_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(project_dir, "TestStack.White.9.2.0.11\\lib\\net40\\"))
-sys.path.append(os.path.join(project_dir, "Castle.Core.3.1.0\\lib\\net40-client\\"))
+sys.path.append(os.path.join(project_dir, "TestStack.White.0.13.3\\lib\\net40\\"))
+sys.path.append(os.path.join(project_dir, "Castle.Core.3.3.0\lib\\net40-client\\"))
+# sys.path.append("C:\\ironpython_training\\TestStack.White.0.13.3\\lib\\net40\\")
+# sys.path.append("C:\\ironpython_training\\Castle.Core.3.3.0\lib\\net40-client\\")
 clr.AddReferenceByName('TestStack.White')
 
 from TestStack.White import Application
